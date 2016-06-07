6e8b7e6bb8c7be08962c7684b1014cf19de9af6a
Wang Cong <ousatoru@hotmail.com>
2016-06-07 19:19:56 +0800

    delegate

--- six/six.cs
+++ six/six.cs
@@ -14,43 +14,39 @@ namespace six
         [STAThreadAttribute]
         static void Main(string[] args)
         {
-            Fiddler.FiddlerApplication.BeforeRequest += new SessionStateHandler(FiddlerApplication_BeforeRequest);
-            Fiddler.FiddlerApplication.BeforeResponse += new Fiddler.SessionStateHandler(FiddlerApplication_BeforeResponse);
-            Fiddler.FiddlerApplication.Startup(8877, FiddlerCoreStartupFlags.Default);
-            Console.ReadKey();
-            Fiddler.FiddlerApplication.Shutdown();
-            Thread.Sleep(500);
-        }
-
-        private static void FiddlerApplication_BeforeRequest(Session oSession)
-        {
-            Console.WriteLine("Before request for:\t" + oSession.fullUrl);
-            Debug.WriteLine(oSession.RequestBody);
-            oSession.bBufferResponse = true;
-        }
-
-        static void FiddlerApplication_BeforeResponse(Session oSession)
-        {
-            if (oSession.fullUrl.Contains("66.185.64.100:8080"))//66.185.64.100:8080
+            Fiddler.FiddlerApplication.BeforeRequest += delegate (Fiddler.Session oSession)
             {
-                oSession.utilDecodeResponse();
-                Console.WriteLine("Before response for:\t" + oSession.fullUrl);
-                string requestText = oSession.GetResponseBodyAsString();
-                Console.WriteLine(requestText);
-                if (requestText.Contains("invigilate.js"))
+                Console.WriteLine("Before request for:\t" + oSession.fullUrl);
+                Debug.WriteLine(oSession.RequestBody);
+                oSession.bBufferResponse = true;
+            };
+            Fiddler.FiddlerApplication.BeforeResponse += delegate (Fiddler.Session oSession)
+            {
+                if (oSession.fullUrl.Contains("66.185.64.100:8080"))//66.185.64.100:8080
                 {
-                    requestText = requestText.Replace("<script type=\"text/javascript\" src=\"../../../../resources/scripts/proj/invigilate.js\"></script>", "");
-                    oSession.utilSetResponseBody(requestText);
+                    oSession.utilDecodeResponse();
+                    Console.WriteLine("Before response for:\t" + oSession.fullUrl);
+                    string requestText = oSession.GetResponseBodyAsString();
                     Console.WriteLine(requestText);
+                    if (requestText.Contains("invigilate.js"))
+                    {
+                        requestText = requestText.Replace("<script type=\"text/javascript\" src=\"../../../../resources/scripts/proj/invigilate.js\"></script>", "");
+                        oSession.utilSetResponseBody(requestText);
+                        Console.WriteLine("Changed to: " + requestText);
+                    }
+                    if (requestText.StartsWith("[{\"id\":null"))
+                    {
+                        string res = requestText;
+                        Clipboard.SetText(res);
+                        oSession.utilSetResponseBody(requestText);
+                        Console.WriteLine("Catching question json: " + res);
+                    }
                 }
-                if (requestText.StartsWith("[{\"id\":null"))
-                {
-                    string res = requestText;
-                    Clipboard.SetText(res);
-                    oSession.utilSetResponseBody(requestText);
-                    Console.WriteLine("Catching question json: " + res);
-                }
-            }
+                Fiddler.FiddlerApplication.Startup(8877, FiddlerCoreStartupFlags.Default);
+                Console.ReadKey();
+                Fiddler.FiddlerApplication.Shutdown();
+                Thread.Sleep(500);
+            };
         }
     }
 }
