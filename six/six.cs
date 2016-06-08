90cc7f5e128db904fe2cc44266aeb446dceb30de
Wang Cong <ousatoru@hotmail.com>
2016-06-09 00:03:33 +0800

    add response code and CLEANIE

--- six/six.cs
+++ six/six.cs
@@ -25,7 +25,7 @@ namespace six
                 if (oSession.fullUrl.Contains("66.185.64.100:8080"))//66.185.64.100:8080
                 {
                     oSession.utilDecodeResponse();
-                    Console.WriteLine("Before response for:\t" + oSession.fullUrl);
+                    Console.WriteLine("Before response for:\t{0}\nresponse code:\t{1}", oSession.fullUrl, oSession.responseCode);
                     string requestText = oSession.GetResponseBodyAsString();
                     Console.WriteLine(requestText);
                     if (requestText.Contains("invigilate.js"))
@@ -36,17 +36,35 @@ namespace six
                     }
                     if (requestText.StartsWith("[{\"id\":null"))
                     {
+                        Console.WriteLine("Catching question json...");
                         string res = requestText;
                         Clipboard.SetText(res);
-                        oSession.utilSetResponseBody(requestText);
-                        Console.WriteLine("Catching question json: " + res);
+                        //oSession.utilSetResponseBody(requestText);
                     }
                 }
             };
+            CleanIE();
             Fiddler.FiddlerApplication.Startup(8877, FiddlerCoreStartupFlags.Default);
             Console.ReadKey();
             Fiddler.FiddlerApplication.Shutdown();
             Thread.Sleep(500);
+        }
+        static void RunCmd(string cmd)
+        {
+            ProcessStartInfo p = new ProcessStartInfo();
+            p.FileName = "cmd.exe";
+            p.Arguments = "/c " + cmd;
+            p.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(p);
+        }
+        static void CleanIE()
+        {
+            /*
+            1.History 2.Cookies 8.Temporary Internet Files
+            16.Form Data 32.Passwords 255.Delete All
+            4351.Delete All - "Also delete files and settings stored by add-ons"
+            */
+            RunCmd("RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8");
         }
     }
 }
